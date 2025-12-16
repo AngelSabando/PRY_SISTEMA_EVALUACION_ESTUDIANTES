f1da2afef57b63d29428db16db8f1fe2221af5f4
AngelSabando <ajsabando@espe.edu.ec>
2025-12-16 17:56:44 -0500

    JS

--- public/js/calculoNotas.js
+++ public/js/calculoNotas.js
@@ -1,46 +1,45 @@
-let notas = [8, 4, 10, 6, 3, 9, 5, 7, 2];
 
-let countAprobados = 0;
-let countSupletorios = 0;
-let countReprobados = 0;
-let sumaNotas = 0;
+var notas = [8, 4, 10, 6, 3, 9, 5, 7, 2];
 
+var txt_aprobados = document.getElementById('txt_aprobados');
+var txt_supletorios = document.getElementById('txt_supletorios');
+var txt_reprobados = document.getElementById('txt_reprobados');
+var txt_promedio = document.getElementById('txt_promedio');
+var txt_estado = document.getElementById('txt_estado');
 
-notas.forEach(function(nota) {
-    
-    sumaNotas += nota;
+calcularCurso();
 
-    if (nota >= 7 && nota <= 10) {
-        countAprobados++; 
-    } else if (nota >= 5 && nota < 7) {
-        countSupletorios++; 
+function calcularCurso() {
+
+    let aprobados = 0;
+    let supletorios = 0;
+    let reprobados = 0;
+    let sumaTotal = 0;
+
+    notas.forEach(function (nota) {
+        sumaTotal = sumaTotal + nota;
+
+        if (nota >= 7) {
+            aprobados++;
+        } else if (nota >= 5) {
+            supletorios++;
+        } else {
+            reprobados++;
+        }
+    });
+
+    let promedio = sumaTotal / notas.length;
+
+    txt_aprobados.textContent = aprobados;
+    txt_supletorios.textContent = supletorios;
+    txt_reprobados.textContent = reprobados;
+    txt_promedio.textContent = promedio.toFixed(2);
+
+    if (promedio >= 7) {
+        txt_estado.textContent = "CURSO APROBADO";
+        txt_estado.className = "text-success fw-bold";
     } else {
-        countReprobados++; 
+        txt_estado.textContent = "CURSO EN RIESGO";
+        txt_estado.className = "text-danger fw-bold";
     }
-});
-
-let promedio = sumaNotas / notas.length;
-
-let estadoTexto = "";
-let claseEstado = "";
-
-if (promedio >= 7) {
-    estadoTexto = "APROBADO";
-    claseEstado = "text-success"; 
-} else {
-    estadoTexto = "EN RIESGO";
-    claseEstado = "text-danger"; 
 }
-
-
-document.getElementById("totalAprobados").innerText = countAprobados;
-
-document.getElementById("totalSupletorios").innerText = countSupletorios;
-
-document.getElementById("totalReprobados").innerText = countReprobados;
-
-document.getElementById("promedioCurso").innerText = promedio.toFixed(2);
-
-let elementoEstado = document.getElementById("estadoCurso");
-elementoEstado.innerText = estadoTexto;
-elementoEstado.className = "fw-bold " + claseEstado; 
